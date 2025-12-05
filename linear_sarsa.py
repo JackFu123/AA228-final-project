92a223daa542cd009ba49886af1f108c654ce0a7
Yiqun Fu <yiqunf@nvidia.com>
2025-12-05 23:50:34 +0800

    [Sarsa] Add yaw acc for action

--- linear_sarsa.py
+++ linear_sarsa.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-epsilon, alpha, gamma = 0.1, 1e-2, 0.99
+alpha, gamma = 5e-2, 0.99
 
 
 def featurize(state: np.ndarray):
@@ -9,31 +9,52 @@ def featurize(state: np.ndarray):
     X_MAX, Y_MAX = 20.0, 20.0
     s[:, 0] = np.clip(s[:, 0], -X_MAX, X_MAX) / X_MAX
     s[:, 1] = np.clip(s[:, 1], -Y_MAX, Y_MAX) / Y_MAX
-    s[:, 2] = s[:, 2] / np.pi  # scale to [-1, 1]
-    return s.flatten()
+    s[:, 2] = s[:, 2] / np.pi  # scale heading to ~[-1, 1]
+    flat = s.flatten()
+    # bias term
+    flat = np.concatenate([flat, np.array([1.0], dtype=flat.dtype)], axis=0)
+    return flat
 
 
 def discretize_action(action):
     acc = action['acceleration']
     yaw_acc = action['yaw_acceleration']
+    dis_acc, dis_yaw_acc = None, None
     if acc > 3:
-        return 0
+        dis_acc = 0
     elif acc > 1:
-        return 1
+        dis_acc = 1
     elif acc > -1:
-        return 2
+        dis_acc = 2
     elif acc > -3:
-        return 3
+        dis_acc = 3
     else:
-        return 4
+        dis_acc = 4
+    
+    if yaw_acc > 0.5:
+        dis_yaw_acc = 5
+    elif yaw_acc > 0.1:
+        dis_yaw_acc = 6
+    elif yaw_acc > -0.1:
+        dis_yaw_acc = 7
+    elif yaw_acc > -0.5:
+        dis_yaw_acc = 8
+    else:
+        dis_yaw_acc = 9
+
+    return dis_acc, dis_yaw_acc
 
 
 def sarsa_linear_update(theta, state, action, reward, next_state, next_action):
     s = featurize(state)
     s_prime = featurize(next_state)
-    action = discretize_action(action)
-    next_action = discretize_action(next_action)
-    q_sa = theta[action] @ s
-    delta = reward + gamma * (theta[next_action] @ s_prime) - q_sa
-    theta[action] += alpha * delta * s   # semi-gradient step
-    return delta
+    action, action_yaw = discretize_action(action)
+    next_action, next_action_yaw = discretize_action(next_action)
+    q_sa_acc = theta[action] @ s
+    delta_acc = reward + gamma * (theta[next_action] @ s_prime) - q_sa_acc
+    theta[action] += alpha * delta_acc * s   # semi-gradient step
+    q_sa_yaw = theta[action_yaw] @ s
+    delta_yaw = reward + gamma * (theta[next_action_yaw] @ s_prime) - q_sa_yaw
+    theta[action_yaw] += alpha * delta_yaw * s   # semi-gradient step
+
+    return delta_acc + delta_yaw
--- train.py
+++ train.py
@@ -3,21 +3,30 @@ from linear_sarsa import sarsa_linear_update
 import numpy as np
 import time
 
+
 if __name__ == "__main__":
     start_time = time.time()
-    print("Start time: ", start_time)
+    print(f"Start time: {time.strftime("%Y-%m-%d %H:%M:%S")}")
     dataset_train = SarsaDataset("/Users/yiqunf/Documents/cousess/AA228/final_project/INTERACTION-Dataset-DR-single-v1_2/train/")
     dataset_val = SarsaDataset("/Users/yiqunf/Documents/cousess/AA228/final_project/INTERACTION-Dataset-DR-single-v1_2/val/")
-    theta = np.random.randn(5, 12)
-    for i in range(1000):
-        sum_delta = 0
+    # 13 features (4 agents x [x, y, psi]) + 1 bias
+    theta = np.random.randn(10, 13)
+    epsilon = 0.1
+    for i in range(100):
+        sum_abs = 0.0
+        sum_sq = 0.0
         for instance in dataset_train:
             delta = sarsa_linear_update(theta, instance['features'], instance['action'], instance['reward'], instance['feature_next'], instance['next_action'])
-            sum_delta += delta
-        print("average delta: ", sum_delta / len(dataset_train))
-        if abs(sum_delta / len(dataset_train)) < 0.01:
+            sum_abs += abs(delta)
+            sum_sq += float(delta) * float(delta)
+        n = len(dataset_train)
+        avg_abs = sum_abs / n
+        mse = sum_sq / n
+        print(f"{time.strftime("%Y-%m-%d %H:%M:%S")} iteration: {i}")
+        print(f"mean delta: {avg_abs} | mse delta: {mse}")
+        if avg_abs < epsilon:
             break
     print(theta)
     end_time = time.time()
-    print("End time: ", end_time)
-    print("Time taken: ", end_time - start_time)
+    print(f"End time: {time.strftime("%Y-%m-%d %H:%M:%S")}")
+    print("Time taken(seconds): ", time.time() - start_time)
